22cb5f7fcd6b4e96d73a0cadc4b7f4ebaaec342a
Алексей <@PC>
2019-07-09 02:37:23 +0500

    Добавлены элементы интерфейса для всех ключевых значений, необходимых для однозначной инициализации начальной системы.

--- MapOfModes/SettingForm.cs
+++ MapOfModes/SettingForm.cs
@@ -18,6 +18,8 @@ namespace MapOfModes
 		public TextBox rStart { get; }
 		public TextBox nuStart { get; }
 		public TextBox kStart { get; } 
+		public TextBox horizontalValueEnd { get; }
+		public TextBox verticalValueEnd { get; }
 
 		public SettingForm()
 		{
@@ -29,8 +31,59 @@ namespace MapOfModes
 
 			this.PrStart = new TextBox
 			{
-				Location = new Point(50, 150),
-				Size = new Size(50, 30),
+				Location = new Point(110, 65),
+				Size = new Size(50, 10),
+			};
+			Label PrLabel = new Label
+			{
+				Location = new Point(5, 65),
+				Text = "Число Прандтля Pr",
+			};
+
+			this.eStart = new TextBox
+			{
+				Location = new Point(110, 90),
+				Size = new Size(50, 10),
+			};
+			Label eLabel = new Label
+			{
+				Location = new Point(10, 90),
+				Text = "Электрическое число Рэлея e",
+				Visible = true,
+			};
+
+			this.rStart = new TextBox
+			{
+				Location = new Point(110, 120),
+				Size = new Size(50, 10),
+			};
+			Label rLabel = new Label
+			{
+				Location = new Point(10, 120),
+				Text = "Тепловое число Рэлея r",
+				Visible = true,
+			};
+
+			this.nuStart = new TextBox
+			{
+				Location = new Point(110, 150),
+				Size = new Size(50, 10),
+			};
+			Label nuLabel = new Label
+			{
+				Location = new Point(10, 150),
+				Text = "Частота nu",
+			};
+
+			this.kStart = new TextBox
+			{
+				Location = new Point(110, 170),
+				Size = new Size(50, 10),
+			};
+			Label kLabel = new Label
+			{
+				Location = new Point(10, 170),
+				Text = "Волновое число k",
 			};
 
 			this.cancel = new Button
@@ -52,7 +105,7 @@ namespace MapOfModes
 
 			this.mode = new ComboBox
 			{
-				Location = new Point(40, 50),
+				Location = new Point(30, 35),
 				Size = new Size(50, 20),
 				Visible = true,
 				Text = "X",
@@ -60,7 +113,7 @@ namespace MapOfModes
 
 			this.horizontalValue = new ComboBox
 			{
-				Location = new Point(95, 50),
+				Location = new Point(85, 35),
 				Size = new Size(50, 20),
 				Visible = true,
 				Text = "Pr",
@@ -68,7 +121,7 @@ namespace MapOfModes
 
 			this.verticalValue = new ComboBox
 			{
-				Location = new Point(190, 50),
+				Location = new Point(180, 35),
 				Size = new Size(50, 20),
 				Visible = true,
 				Text = "Pr",
@@ -76,34 +129,70 @@ namespace MapOfModes
 
 			Label modeLabel = new Label
 			{
-				Location = new Point(45, 30),
+				Location = new Point(35, 15),
 				Size = new Size(50, 30),
-				Visible = true,
 				Text = "Мода",
 
 			};
 
 			Label horizontalValueLabel = new Label
 			{
-				Location = new Point(95, 20),
+				Location = new Point(85, 5),
 				Size = new Size(95, 30),
-				Visible = true,
 				Text = "Горизонтальный параметр",
 			};
 
 			Label verticalValueLabel = new Label
 			{
-				Location = new Point(190, 20),
+				Location = new Point(180, 5),
 				Size = new Size(120, 30),
-				Visible = true,
 				Text = "Вертикальный параметр",
 			};
+
+			Label finalNumbers = new Label
+			{
+				Location = new Point(205, 78),
+				Text = "Конечное значение",
+			};
+
+			this.horizontalValueEnd = new TextBox
+			{
+				Location = new Point(210, 120),
+				Size = new Size(50, 10),
+			};
+			Label horizontalValueEndLabel = new Label
+			{
+				Location = new Point(205, 105),
+				Text = "по горизонтали:",
+			};
+
+			this.verticalValueEnd = new TextBox
+			{
+				Location = new Point(210, 155),
+				Size = new Size(50, 10),
+			};
+			Label verticalValueEndLabel = new Label
+			{
+				Location = new Point(205, 140),
+				Text = "по вертикали:",
+			};
+
+
 
 			mode.Items.AddRange(new string[] { "X", "Y", "Z", "V", "W" });
 			horizontalValue.Items.AddRange(new string[] { "Pr", "nu", "e", "r", "k" });
 			verticalValue.Items.AddRange(new string[] { "Pr", "nu", "e", "r", "k" });
 
+			Controls.Add(PrLabel);
 			Controls.Add(PrStart);
+			Controls.Add(eLabel);
+			Controls.Add(eStart);
+			Controls.Add(rLabel);
+			Controls.Add(rStart);
+			Controls.Add(nuLabel);
+			Controls.Add(nuStart);
+			Controls.Add(kLabel);
+			Controls.Add(kStart);
 			Controls.Add(accept);
 			Controls.Add(cancel);
 			Controls.Add(mode);
@@ -112,6 +201,11 @@ namespace MapOfModes
 			Controls.Add(modeLabel);
 			Controls.Add(horizontalValueLabel);
 			Controls.Add(verticalValueLabel);
+			Controls.Add(horizontalValueEnd);
+			Controls.Add(finalNumbers);
+			Controls.Add(horizontalValueEndLabel);
+			Controls.Add(verticalValueEnd);
+			Controls.Add(verticalValueEndLabel);
 		}
 	}
 }
